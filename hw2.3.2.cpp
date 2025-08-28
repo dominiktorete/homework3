2d4a8083d4980aeb21f7a875b650e4163558adc7
dominiktorete <emec85748@gmail.com>
2025-08-28 17:44:54 +0500

    Update hw2.3.2.cpp

--- hw2.3.2.cpp
+++ hw2.3.2.cpp
@@ -6,15 +6,13 @@ class Counter{
 public:
 	Counter() : value(1)
 	{}
-	void set_individual_value(int individual_value);
+	Counter(int individual_value) : value(individual_value)
+	{}
 	void increase_count();
 	void decrease_count();
 	int return_value_count();
 
 };
-	void Counter::set_individual_value(int individual_value){
-		value = individual_value;
-	}
 	void Counter::increase_count(){
 		++value;
 	}
@@ -25,25 +23,31 @@ public:
 		return value;
 	}
 
+void function_dublicate(Counter& coun){
+	std::string comm{};
+	do{
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> comm;
+		if(comm == "+") coun.increase_count();
+		else if(comm == "-") coun.decrease_count();
+		else if(comm == "=") std::cout << coun.return_value_count() << std::endl;
+	}while(comm != "x" && comm != "х");
+}
 int main(){
 	setlocale(LC_ALL, "ru");
 	int ind_val{};
 	std::string command{};
-	Counter count;
 	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
 	std::cin >> command;
 	if(command == "Да" || command == "да"){
 		std::cout << "Введите начальное значение счётчика: ";
 		std::cin >> ind_val;
-		count.set_individual_value(ind_val);
+		Counter count(ind_val);
+		function_dublicate(count);
 	}
-	do{
-		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-		std::cin >> command;
-		if(command == "+") count.increase_count();
-		else if(command == "-") count.decrease_count();
-		else if(command == "=") std::cout << count.return_value_count() << std::endl;
-	}while(command != "x" && command != "х");
+	else{ Counter count;
+		function_dublicate(count);
+	}
 	std::cout << "До свидания!";
 	return 0;
 }
