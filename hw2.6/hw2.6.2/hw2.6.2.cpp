85370bb9963cc1de9b3cbe719aa7eddca9dc9efc
dominiktorete <emec85748@gmail.com>
2025-09-06 16:07:35 +0500

    Update hw2.6.2.cpp

--- hw2.6/hw2.6.2/hw2.6.2.cpp
+++ hw2.6/hw2.6.2/hw2.6.2.cpp
@@ -1,37 +1,38 @@
-// #include <iostream>
-// #include <locale>
-// #include <Windows.h>
-// #include "counter.h"
-// void func_io(Counter& cr){
-//     char ch{};
-//     do{
-//         std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-//         std::cin >> ch;
-//         if(ch == '+') cr.countUP();
-//         else if(ch == '-')cr.countDown();
-//         else if(ch == '=')std::cout << cr.get_count() << std::endl;
-//         else if(ch == 'x' || ch == 'х') std::cout << "До свидания!" << std::endl;
-//         else std::cout << "Такой команды нет!!!" << std::endl;
-//     }while(ch != 'x' && ch != 'х');
-// }
-// int main(){
-//     setlocale(LC_ALL, "ru");
-//     SetConsoleCP(1251);
-//     SetConsoleOutputCP(1251);
-//     std::string command{};
-//     std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-//     std::cin >> command;
-//     if(command == "Да" || command == "да" || command == "ДА"){
-//         std::cout << "Ввведите начальное значение: ";
-//         int number{};
-//         std::cin >> number;
-//         Counter c1(number);
-//         func_io(c1);
-//     }
-//     else if(command == "Нет" || command == "нет" || command == "НЕТ"){
-//         Counter c2;
-//         func_io(c2);
-//     }
-//     else std::cout << "Я вас не понимаю...";
-//     return 0;
-// }
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include "counter.h"
+void func_io(Counter& cr){
+    char ch{};
+    do{
+        std::cout << "Г‚ГўГҐГ¤ГЁГІГҐ ГЄГ®Г¬Г Г­Г¤Гі ('+', '-', '=' ГЁГ«ГЁ 'x'): ";
+        std::cin >> ch;
+        if(ch == '+') cr.countUP();
+        else if(ch == '-')cr.countDown();
+        else if(ch == '=')std::cout << cr.get_count() << std::endl;
+        else if(ch == 'x' || ch == 'Гµ') std::cout << "Г„Г® Г±ГўГЁГ¤Г Г­ГЁГї!" << std::endl;
+        else std::cout << "Г’Г ГЄГ®Г© ГЄГ®Г¬Г Г­Г¤Г» Г­ГҐГІ!!!" << std::endl;
+    }while(ch != 'x' && ch != 'Гµ');
+}
+int main(){
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string command{};
+    std::cout << "Г‚Г» ГµГ®ГІГЁГІГҐ ГіГЄГ Г§Г ГІГј Г­Г Г·Г Г«ГјГ­Г®ГҐ Г§Г­Г Г·ГҐГ­ГЁГҐ Г±Г·ВёГІГ·ГЁГЄГ ? Г‚ГўГҐГ¤ГЁГІГҐ Г¤Г  ГЁГ«ГЁ Г­ГҐГІ: ";
+    std::cin >> command;
+    if(command == "Г„Г " || command == "Г¤Г " || command == "Г„ГЂ"){
+        std::cout << "Г‚ГўГўГҐГ¤ГЁГІГҐ Г­Г Г·Г Г«ГјГ­Г®ГҐ Г§Г­Г Г·ГҐГ­ГЁГҐ: ";
+        int number{};
+        std::cin >> number;
+        Counter c1(number);
+        func_io(c1);
+    }
+    else if(command == "ГЌГҐГІ" || command == "Г­ГҐГІ" || command == "ГЌГ…Г’"){
+        Counter c2;
+        func_io(c2);
+    }
+    else std::cout << "Гџ ГўГ Г± Г­ГҐ ГЇГ®Г­ГЁГ¬Г Гѕ...";
+    return 0;
+}
+
