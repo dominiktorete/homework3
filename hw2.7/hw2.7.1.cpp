719790fb8733537a30ee5c8b5a528c0b484a7a8b
dominiktorete <emec85748@gmail.com>
2025-09-10 14:38:57 +0500

    Update hw2.7.1.cpp

--- hw2.7/hw2.7.1.cpp
+++ hw2.7/hw2.7.1.cpp
@@ -1,33 +1,34 @@
-// #include <iostream>
-// #include <Windows.h>
-// #include <locale>
-// #define MODE 1
-// #ifdef MODE
-//     #if(MODE == 1)
-//         int add(int a, int b){
-//         return a + b;
-//     }
-//     #endif
-//     int main(){
-//         setlocale(LC_ALL, "ru");
-//         SetConsoleCP(1251);
-//         SetConsoleOutputCP(1251);
-//     #if(MODE == 1)
-//         int a{}, b{};
-//         std::cout << "Ðàáîòàþ â áîåâîì ðåæèìå" << std::endl;
-//         std::cout << "Ââåäèòå ïåðâîå ÷èñëî: ";
-//         std::cin >> a;
-//         std::cout << "Ââåäèòå âòîðîå ÷èñëî: ";
-//         std::cin >> b;
-//         std::cout << "Ðåçóëüòàò ñëîæåíèÿ: " << add(a,b) << std::endl;
-//     #elif(MODE == 0)
-//         std::cout << "Ðàáîòàþ â ðåæèìå òðåíèðîâêè" << std::endl;
-//     #else
-//         std::cout << "Íåèçâåñòíûé ðåæèì. Çàâåðøåíèå ðàáîòû!" << std::endl;
+#include <iostream>
+#include <Windows.h>
+#include <locale>
+#define MODE 1
+#ifdef MODE
+    #if(MODE == 1)
+        int add(int a, int b){
+        return a + b;
+    }
+    #endif
+    int main(){
+        setlocale(LC_ALL, "ru");
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+    #if(MODE == 1)
+        int a{}, b{};
+        std::cout << "ÃÃ Ã¡Ã®Ã²Ã Ã¾ Ã¢ Ã¡Ã®Ã¥Ã¢Ã®Ã¬ Ã°Ã¥Ã¦Ã¨Ã¬Ã¥" << std::endl;
+        std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã¥Ã°Ã¢Ã®Ã¥ Ã·Ã¨Ã±Ã«Ã®: ";
+        std::cin >> a;
+        std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¢Ã²Ã®Ã°Ã®Ã¥ Ã·Ã¨Ã±Ã«Ã®: ";
+        std::cin >> b;
+        std::cout << "ÃÃ¥Ã§Ã³Ã«Ã¼Ã²Ã Ã² Ã±Ã«Ã®Ã¦Ã¥Ã­Ã¨Ã¿: " << add(a,b) << std::endl;
+    #elif(MODE == 0)
+        std::cout << "ÃÃ Ã¡Ã®Ã²Ã Ã¾ Ã¢ Ã°Ã¥Ã¦Ã¨Ã¬Ã¥ Ã²Ã°Ã¥Ã­Ã¨Ã°Ã®Ã¢ÃªÃ¨" << std::endl;
+    #else
+        std::cout << "ÃÃ¥Ã¨Ã§Ã¢Ã¥Ã±Ã²Ã­Ã»Ã© Ã°Ã¥Ã¦Ã¨Ã¬. Ã‡Ã Ã¢Ã¥Ã°Ã¸Ã¥Ã­Ã¨Ã¥ Ã°Ã Ã¡Ã®Ã²Ã»!" << std::endl;
 
-//     #endif
-//     }
-// #endif
-// #ifndef MODE
-//     #error Define MODE.
-// #endif
+    #endif
+    }
+#endif
+#ifndef MODE
+    #error Define MODE.
+#endif
+
