411ba466ff0c3f65ed6ec891d4f52b6561498056
dominiktorete <emec85748@gmail.com>
2025-09-26 13:14:22 +0300

    Add files via upload

---
+++ 2.9/Nod_2.h
@@ -0,0 +1,4 @@
+#ifndef NOD_2_H
+#define NOD_2_H
+int nod2(int, int);
+#endif // NOD_2_H
---
+++ 2.9/hw2.9.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "fraction_2.h"
+
+int main()
+{
+    Fraction f1(8, 12);
+    Fraction f2(6, 12);
+    std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+    return 0;
+}
